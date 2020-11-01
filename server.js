0ca0d329c14dfab61ad901d4427ea4fd9c8b430c
OsamaSR <71220089+OsamaSR@users.noreply.github.com>
2020-11-01 11:39:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const scraper = require('./scraper');
 
 const app = express();
 //Osama
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //app.use(cors());
 //Home page
 app.get('/', (req, res) => {
